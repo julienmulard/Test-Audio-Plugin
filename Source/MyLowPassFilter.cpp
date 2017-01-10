8dae3b4fe483d37a6e740bd31362e2860b765885
Julien Mulard <julien.mulard@gmail.com>
2017-01-10 14:32:34 +0100

    MyLowPassFilter hérite maintenant de MyFilter

--- Source/MyLowPassFilter.cpp
+++ Source/MyLowPassFilter.cpp
@@ -1,54 +1,14 @@
 #include "MyLowPassFilter.h"
 
-MyLowPassFilter::MyLowPassFilter(float cutoff, float Q, float sampleRate)
-{
-	mSampleRate = sampleRate;
-	mCutoffFrequency = cutoff;
-	mQ = Q;
 
-	m1_Fs = 1 / mSampleRate;
-	m1_Q = 1 / mQ;
+#include "MyLowPassFilter.h"
 
-	mComputeFilterCoefficients();
-
-	mMemIn[0] = 0.0f;
-	mMemIn[1] = 0.0f;
-	mMemOut[0] = 0.0f;
-	mMemOut[1] = 0.0f;
-}
-
-MyLowPassFilter::~MyLowPassFilter()
-{
+MyLowPassFilter::MyLowPassFilter(float cutoff, float Q, float sampleRate) : MyFilter(cutoff, Q, sampleRate) {
 
 }
 
-void MyLowPassFilter::setCutoff(float cutoff)
-{
-	mCutoffFrequency = cutoff;
-	mComputeFilterCoefficients();
-}
+MyLowPassFilter::~MyLowPassFilter() {
 
-void MyLowPassFilter::setQ(float Q)
-{
-	mQ = Q;
-	m1_Q = 1.0f / mQ;
-	mComputeFilterCoefficients();
-}
-
-void MyLowPassFilter::setFilter(float cutoff, float Q)
-{
-	mCutoffFrequency = cutoff;
-	mQ = Q;
-	m1_Q = 1.0f / mQ;
-	mComputeFilterCoefficients();
-}
-
-
-void MyLowPassFilter::setSampleRate(float sampleRate)
-{
-	mSampleRate = sampleRate;
-	m1_Fs = 1.0f / mSampleRate;
-	mComputeFilterCoefficients();
 }
 
 void MyLowPassFilter::mComputeFilterCoefficients()
@@ -66,19 +26,5 @@ void MyLowPassFilter::mComputeFilterCoefficients()
 	mA[0] = 1.0f + mAlpha;
 	mA[1] = -2.0f * mC0;
 	mA[2] = 1.0f - mAlpha;
-}
-
-
-float MyLowPassFilter::filter(float input) 
-{
-	float output = (mB[0] * input + mB[1] * mMemIn[0] + mB[2] * mMemIn[1] - mA[1] * mMemOut[0] - mA[2] * mMemOut[1]) / mA[0] +10e-20f;
-
-	mMemIn[1] = mMemIn[0];
-	mMemIn[0] = input;
-
-	mMemOut[1] = mMemOut[0];
-	mMemOut[0] = output;
-
-	return output;
 }
 
