941efd4f9be983643ff45f11e0f56c1171080dd1
Julien Mulard <julien.mulard@gmail.com>
2017-01-10 13:57:52 +0100

    MyHighPassFilter hérite maintenant de MyFilter pour un code plus lisible.

--- Source/MyHighPassFilter.cpp
+++ Source/MyHighPassFilter.cpp
@@ -1,55 +1,16 @@
 #include "MyHighPassFilter.h"
 
-MyHighPassFilter::MyHighPassFilter(float cutoff, float Q, float sampleRate)
-{
-	mSampleRate = sampleRate;
-	mCutoffFrequency = cutoff;
-	mQ = Q;
 
-	m1_Fs = 1 / mSampleRate;
-	m1_Q = 1 / mQ;
+#include "MyHighPassFilter.h"
 
-	mComputeFilterCoefficients();
-
-	mMemIn[0] = 0.0f;
-	mMemIn[1] = 0.0f;
-	mMemOut[0] = 0.0f;
-	mMemOut[1] = 0.0f;
-}
-
-MyHighPassFilter::~MyHighPassFilter()
-{
+MyHighPassFilter::MyHighPassFilter(float cutoff, float Q, float sampleRate) : MyFilter(cutoff, Q, sampleRate) {
 
 }
 
-void MyHighPassFilter::setCutoff(float cutoff)
-{
-	mCutoffFrequency = cutoff;
-	mComputeFilterCoefficients();
+MyHighPassFilter::~MyHighPassFilter() {
+
 }
 
-void MyHighPassFilter::setQ(float Q)
-{
-	mQ = Q;
-	m1_Q = 1.0f / mQ;
-	mComputeFilterCoefficients();
-}
-
-void MyHighPassFilter::setFilter(float cutoff, float Q)
-{
-	mCutoffFrequency = cutoff;
-	mQ = Q;
-	m1_Q = 1.0f / mQ;
-	mComputeFilterCoefficients();
-}
-
-
-void MyHighPassFilter::setSampleRate(float sampleRate)
-{
-	mSampleRate = sampleRate;
-	m1_Fs = 1.0f / mSampleRate;
-	mComputeFilterCoefficients();
-}
 
 void MyHighPassFilter::mComputeFilterCoefficients()
 {
@@ -69,16 +30,5 @@ void MyHighPassFilter::mComputeFilterCoefficients()
 }
 
 
-float MyHighPassFilter::filter(float input)
-{
-	float output = (mB[0] * input + mB[1] * mMemIn[0] + mB[2] * mMemIn[1] - mA[1] * mMemOut[0] - mA[2] * mMemOut[1]) / mA[0] + 10e-20f;
 
-	mMemIn[1] = mMemIn[0];
-	mMemIn[0] = input;
-
-	mMemOut[1] = mMemOut[0];
-	mMemOut[0] = output;
-
-	return output;
-}
 
